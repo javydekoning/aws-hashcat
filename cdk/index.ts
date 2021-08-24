3e4f44131acb1e748cab89a293d387a1126226ff
Javy de Koning <javy@amazon.nl>
2021-08-24 14:35:44 +0200

    First pipeline iteration

---
+++ cdk/index.ts
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as iam from '@aws-cdk/aws-iam';
+import { readFileSync } from 'fs';
+const bootstrap: string = readFileSync('./userdata/bootstrap.sh', 'utf8');
+
+
+const app = new cdk.App();
+
+class TempHashcatStack extends cdk.Stack{
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    
+    const vpc = new ec2.Vpc(this, 'TempVpc', {
+      maxAzs: 1 
+    })
+
+    const userData = ec2.UserData.forLinux()
+    userData.addCommands(bootstrap)
+
+    const role = new iam.Role(this, 'HashCatEc2Role', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com')
+    })
+    role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'))
+    role.addToPolicy(new iam.PolicyStatement({
+      actions: ['ssm:GetParameter'],
+      effect: iam.Effect.ALLOW,
+      resources: [`arn:aws:ssm:us-east-1:${cdk.Stack.of(this).account}:parameter/gitkey`]
+    }))
+
+    const instance = new ec2.Instance(this, 'Instance', {
+      instanceType: new ec2.InstanceType('g4dn.12xlarge'),
+      vpc,
+      role,
+      machineImage: new ec2.LookupMachineImage({
+        name: 'AWS Deep Learning AMI GPU CUDA 11.3 (Amazon Linux 2)',
+        windows: false,
+        filters: {name: ['*Deep Learning AMI GPU CUDA 11.3*Amazon Linux 2*']},
+        owners: ['amazon']
+      }),
+      allowAllOutbound: true,
+      userData,
+    })
+
+    const cfnInstance = instance.node.defaultChild as ec2.CfnInstance;
+    cfnInstance.addPropertyOverride('InstanceInitiatedShutdownBehavior', 'terminate');
+  }
+}
+new TempHashcatStack(app, 'TempHashCatStack', {
+  env: {region: 'us-east-1', account: process.env.CDK_DEFAULT_ACCOUNT}
+});
---
+++ cdk/userdata/bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+aws ssm get-parameter --name 'gitkey' --with-decryption --region us-east-1 | jq -r '.Parameter.Value' > ~/.ssh/id_rsa
+chmod 0700 ~/.ssh/id_rsa
+ssh -o StrictHostKeyChecking=no git@github.com
+git clone git@github.com:javydekoning/aws-hashcat.git
+cd aws-hashcat
+# PULL IMAGES
+nvidia-docker pull javydekoning/hashcat:cuda
+# RUN CUDA
+export HCVER=$(nvidia-docker run javydekoning/hashcat:cuda hashcat --version)
+export FILE=$(curl http://169.254.169.254/latest/meta-data/instance-type).$HCVER.cuda.txt
+nvidia-docker run javydekoning/hashcat:cuda hashcat -b > $FILE
+git add $FILE
+git commit -a -m "Adding $FILE"
+git push
+#TERMINATE
+sudo shutdown 0
